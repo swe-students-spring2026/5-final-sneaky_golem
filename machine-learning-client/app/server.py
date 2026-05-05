1db17184d26d71e7f356ef161be49179eba75b27
NarezIn <zn2133@nyu.edu>
2026-05-04 20:12:29 -0400

    Update ml-client server logic of extracting board from image"

--- machine-learning-client/app/server.py
+++ machine-learning-client/app/server.py
@@ -39,4 +39,4 @@ def extract_board_route():
     return jsonify({"board": board_matrix})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
